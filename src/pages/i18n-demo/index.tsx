316277eae1aaaed2b95201f8c61afea75be30f47
Hyden Liu <sunupdong@gmail.com>
2025-09-01 16:30:48 +0800

    feat: restructure i18n localization files and update paths

--- src/pages/i18n-demo/index.tsx
+++ src/pages/i18n-demo/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import '@/styles/main.css'
 import { initI18n } from '@/utils/i18n'
-import { resources } from './locals'
+import { resources } from './locales'
 
 initI18n(resources, 'fr')
 
